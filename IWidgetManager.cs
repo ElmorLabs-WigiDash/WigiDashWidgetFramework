03a1038a767f7f78a36ac57da72360f4e8fe4af5
Jon Sandstrom <jon@elmorlabs.com>
2022-03-29 23:31:11 +0800

    Add hardware sensors to IWidgetManager

--- IWidgetManager.cs
+++ IWidgetManager.cs
@@ -9,6 +9,7 @@ namespace FrontierWidgetFramework
 
     public delegate void FullScreenEnteredEventHandler(FullScreenEventArgs e);
     public delegate void FullScreenExitedEventHandler(FullScreenEventArgs e);
+    public delegate void SensorUpdatedEventHandler(SensorItem item, double value);
 
     public interface IWidgetManager
     {
@@ -18,9 +19,33 @@ namespace FrontierWidgetFramework
         // Events
         event FullScreenEnteredEventHandler FullScreenEntered;
         event FullScreenExitedEventHandler FullScreenExited;
+        public event SensorUpdatedEventHandler SensorUpdated;
 
         // Functionality
         bool StoreSetting(IWidgetInstance instance, string name, string value);
         bool LoadSetting(IWidgetInstance instance, string name, out string value);
+
+        // Hardware sensors
+        AddMonitoringItem(SensorItem item);
+        RemoveMonitoringItem(SensorItem item);
+        List<SensorItem> GetSensorList();
+
+    }
+
+    public class SensorItem {
+        
+        public int ReadingType;
+        public int SensorId1;
+        public int SensorId2;
+        public string Name;
+        public string Unit;
+
+        public SensorItem(int reading_type, int sensor_id1, int sensor_id2, string name, string unit) {
+            SensorId1 = sensor_id1;
+            SensorId2 = sensor_id2;
+            ReadingType = reading_type;
+            Name = name;
+            Unit = unit;
+        }
     }
 }
